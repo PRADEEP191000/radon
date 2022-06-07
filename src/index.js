bd356771dc557793ba27787c6ea22cffc3728297
Pradeep Gupta <pradeeepgupta191000@gmail.com>
2022-06-07 21:58:24 +0530

    all

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+B
+mongoose.connect("mongodb+srv://Pradeep-Gupta:faptismld@cluster0.imx3g.mongodb.net/Pradeep191000-DB?retryWrites=true&w=majority", {
+        useNewUrlParser: true
+    })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
 
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
